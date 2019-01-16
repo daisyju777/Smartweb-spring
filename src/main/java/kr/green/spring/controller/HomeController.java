7e88fcd1f451071e698111606684ef6288a39ac6
Unknown <disky32@naver.com>
2019-01-16 17:10:28 +0900

    HomeController.java 수정

--- src/main/java/kr/green/spring/controller/HomeController.java
+++ src/main/java/kr/green/spring/controller/HomeController.java
@@ -18,14 +18,14 @@ public class HomeController {
 	private AccountService accountService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String homeGet(Locale locale, Model model) {
+	public String homeGet(Model model) {
 		/* 컨트롤러에서 jsp로 데이터를 전달하기 위해서
 		 * 1. 매개변수에 Model1 객체를 추가 : Model model
 		 * 2. model.addAttribute()를 통해 데이터 전달
 		 *    model.addAttribute("jsp에서받는이름", 전달할데이터);
 		 */
-		model.addAttribute("serverTime", "서버시간" );  //서버시간이라는 문자열을 전달함
-		System.out.println(accountService.getAccount("stajun"));
+		//model.addAttribute("serverTime", "서버시간" );  //서버시간이라는 문자열을 전달함
+		//System.out.println(accountService.getAccount("stajun"));
 		return "home";
 	}
 	
@@ -37,8 +37,8 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signupGet(Boolean fail, Model model) {
-		if (fail == null) {
+	public String signupGet(Boolean fail, Model model) {          //컨트롤러에 있는 jsp를 보내기 위해 model 매개변수 필요
+		if (fail == null) {           //주소뒤에 아무것도 안붇고 그냥 들어왔을때  http://localhost:8080/spring/signup?fail=false 에서 ?fail=false가 없음
 			fail = false;
 		}
 		model.addAttribute("fail", fail);
@@ -47,7 +47,7 @@ public class HomeController {
 	
 	//int -> Integer, double -> Double, boolean -> Boolean  일반 자료형을 클래스의 객체로 만듬. 이러한 클래스를 rapper class라 한다. null값을 허용할수 있도록 하기 위해서
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signupPOST(AccountVo accountVo, Model model) {
+	public String signupPOST(AccountVo accountVo, Model model) {           //전달받은 내용을 accountVo에 저장. 업무를 마치고 서비스가 나한테 데이터를 전달할때까지 기다림
 		if(accountService.signup(accountVo))
 			return "redirect:/";
 		else {
