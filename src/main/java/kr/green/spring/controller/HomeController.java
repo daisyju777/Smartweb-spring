9702b5f10a5a8677f9c1d9976417802f73ed25ab
Unknown <disky32@naver.com>
2019-01-16 16:36:00 +0900

    DB 연동 및 회원가입 예제 구현

---
+++ src/main/java/kr/green/spring/controller/HomeController.java
@@ -0,0 +1,58 @@
+package kr.green.spring.controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.green.spring.service.AccountService;
+import kr.green.spring.vo.AccountVo;
+
+
+@Controller
+public class HomeController {
+	@Autowired
+	private AccountService accountService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homeGet(Locale locale, Model model) {
+		/* 컨트롤러에서 jsp로 데이터를 전달하기 위해서
+		 * 1. 매개변수에 Model1 객체를 추가 : Model model
+		 * 2. model.addAttribute()를 통해 데이터 전달
+		 *    model.addAttribute("jsp에서받는이름", 전달할데이터);
+		 */
+		model.addAttribute("serverTime", "서버시간" );  //서버시간이라는 문자열을 전달함
+		System.out.println(accountService.getAccount("stajun"));
+		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(String name) {
+		/* jsp에서 넘겨준 데이터를 받기 위해서는 태그의 속성 name과 일치하는 이름을 매개변수의 이름으로 설정한다.*/
+		System.out.println("jsp에서 넘어온 값: " +name);
+		return "redirect:/"; //jsp파일을 직접 부르는게 아니고 내가부르고자하는 URI를 호출하는 값을 다시 호출
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signupGet(Boolean fail, Model model) {
+		if (fail == null) {
+			fail = false;
+		}
+		model.addAttribute("fail", fail);
+		return "account/signup";
+	}
+	
+	//int -> Integer, double -> Double, boolean -> Boolean  일반 자료형을 클래스의 객체로 만듬. 이러한 클래스를 rapper class라 한다. null값을 허용할수 있도록 하기 위해서
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signupPOST(AccountVo accountVo, Model model) {
+		if(accountService.signup(accountVo))
+			return "redirect:/";
+		else {
+			model.addAttribute("fail",true);
+			return "redirect:/signup";
+		}
+	}
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -9,8 +9,15 @@
 	Hello world!  
 </h1>
 
+<%-- 컨트롤러에서 전달한 데이터를 받기위해 ${컨트롤러에서 전달한 이름}을 사용한다. --%>
 <P>  The time on the server is ${serverTime}. </P>
-<form method="post">
+<!-- jsp에서 컨트롤러로 데이터를 전달하려면 form태그를 이용 -->
+<form method="post" action="<%=request.getContextPath() %>/">   
+<!-- 액션에 따로 주소를 지정해 주지 않으면 브라우저 주소창에 입력해준 /?name=홍길동 이 들어감. 유관순을 입력하고 제출하주면
+jsp에서 넘어온 값: 홍길동,유관순 이 콘솔창에 뜸. 그러므로 따로 경로를 지정해줘야함. 프로젝트에 따라 경로가 바뀌므로 경로를
+위와 같이 request.getContextPath() 지정해줌. 그러면 제출한 유관순 하나만 뜨게됨. 
+  -->
+	<!-- 전달하려는 데이터가 있는 태그의 속성name을 입력한다. -->
 	<input type="text" name="name">
 	<button>제출</button>
 </form>
